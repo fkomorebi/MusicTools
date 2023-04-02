e7330eb87f38b6f6e0952823acfd5c1e70690660
DESKTOP-JE30UEN\komorebi <f.komorebi@qq.com>
2023-04-02 17:34:32 +0800

    更改首页样式

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,13 @@
 <template>
   <!--index.wxml-->
   <view class="title">工具:</view>
-  <button @click="onClick('tuner')" size="mini" class="button" style="height: 100rpx;width: 210rpx;">调音器</button>
+  <button @click="onClick('tuner')" size="mini" class="button">调音器</button>
   <!-- <button size="mini" class="button" style="height: 100rpx;width: 210rpx;">鼓机</button>
   <button @click="onClick('metronome')" size="mini" class="button" style="height: 100rpx;width: 210rpx;">节拍器</button> -->
   <view class="title">指板：</view>
   <!-- <button size="mini" class="button" style="height: 100rpx;width: 210rpx;">和弦</button> -->
   <!-- <button size="mini" class="button" style="height: 100rpx;width: 210rpx;">音符</button> -->
-  <button @click="onClick('scale')" size="mini" class="button" style="height: 100rpx;width: 210rpx;">音阶</button>
+  <button @click="onClick('scale')" size="mini" class="button">音阶</button>
   <!-- <view class="title">交流区：</view>
   <button size="mini" class="button" style="height: 100rpx;width: 210rpx;">问题</button>
   <button size="mini" class="button" style="height: 100rpx;width: 210rpx;">心得分享</button> -->
@@ -30,7 +30,7 @@ function onClick(type) {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 .title {
   color: gray;
   font-size: 50rpx;
@@ -38,20 +38,12 @@ function onClick(type) {
 
 .button {
   background-color: #71a193;
-  color: rgb(255, 255, 255)55;
-  text-align: center;
-  text-size-adjust: inherit;
-  margin: 20rpx;
-  line-height: 12rpx;
-
-}
-
-.tuner view {
-  background-color: rgb(0, 195, 255);
-  height: 40px;
-  width: 80px;
-  text-align: center;
-  margin: 20rpx;
-
+  color: rgb(255, 255, 255);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  height: 50px;
+  width: 100px;
 }
 </style>
